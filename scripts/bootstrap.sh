35f64188f7dc1938f92a0515178000e7c8a32e27
SIRE Stéphane <s.sire@oppidoc.fr>
2017-05-09 14:32:25 +0200

    Added /db/sites/pilote collection creation to bootstrap shell script because the initial deploy command in README would cause Permission to modify permissions is denied for user 'admin' on '/db/sites/ctracker' otherwise

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -12,6 +12,7 @@
 APPCOL='ctracker' # edit and put the same value as $globals:app-collection in globals.xqm
 ../../../../bin/client.sh -u admin -P $1 -m "/db/www/$APPCOL/mesh" --parse ../mesh
 ../../../../bin/client.sh -u admin -P $1 -m "/db/www/$APPCOL/config" --parse ../config
+../../../../bin/client.sh -u admin -P $1 -m "/db/sites/$APPCOL" -s
 CURPWD=`pwd`
 CURDIR=`dirname $CURPWD`
 CURMOD=`basename $CURDIR`
@@ -19,3 +20,4 @@ echo "update value fn:doc('/db/www/$APPCOL/config/mapping.xml')/site/@key with '
 echo "update value fn:doc('/db/www/$APPCOL/config/mapping.xml')/site/@db with '/db/sites/$APPCOL'" | ../../../../bin/client.sh -u admin -P $1 -x -s
 echo "update value fn:doc('/db/www/$APPCOL/config/mapping.xml')/site/@confbase with '/db/www/$APPCOL'" | ../../../../bin/client.sh -u admin -P $1 -x -s
 echo "update value fn:doc('/db/www/$APPCOL/config/skin.xml')/*/@key with '$CURMOD'" | ../../../../bin/client.sh -u admin -P $1 -x -s
+
